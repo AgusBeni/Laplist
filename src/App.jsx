98a4fb1458f63abf3c03f2d184be8cf71c22dc34
Agus Zwitser <agus.zwitser@hva.nl>
2025-01-16 00:07:36 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+function App() {
+  return (
+    <>
+      <div className="nav">
+        <h1 className="header">Laplist</h1>
+        <div>
+          <button>Add item</button>
+          <button>Remove item</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
