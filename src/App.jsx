638a0e7056f2b64978981dd96e5416f1b7ec4a5e
Agus Zwitser <agus.zwitser@hva.nl>
2025-01-18 22:13:36 +0100

    feat: updated logic for adding new laptop to liist from a form

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,18 @@ import { useState } from "react";
 function App() {
   const [toggleForm, setToggleForm] = useState(false);
 
-  // const [laptopForm, setLaptopForm] = useState({});
+  const [laptopList, setLaptopList] = useState([]);
 
   function changeToggle() {
     if (toggleForm == true) return setToggleForm(false);
     return setToggleForm(true);
   }
 
-  // function handleSubmit(e, ) {
-  //   e.preventDefault();
-
-
-  // }
+  function addNewForm(newForm) {
+    setLaptopList((currentLaptopList) => {
+      return [...currentLaptopList, { id: crypto.randomUUID(), ...newForm }];
+    });
+  }
 
   return (
     <>
@@ -45,49 +45,27 @@ function App() {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>Lenovo</td>
-                  <td>i7</td>
-                  <td>SSD</td>
-                  <td>1TB</td>
-                  <td>Nvidia RTX 2060</td>
-                  <td>4GB</td>
-                  <td>16GB</td>
-                  <td>
-                    <button className="action-button edit">Edit</button>
-                    <button className="action-button delete">Delete</button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Asus</td>
-                  <td>i5</td>
-                  <td>SSD</td>
-                  <td>2TB</td>
-                  <td>Nvidia RTX 2050</td>
-                  <td>2GB</td>
-                  <td>8GB</td>
-                  <td>
-                    <button className="action-button edit">Edit</button>
-                    <button className="action-button delete">Delete</button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Dell</td>
-                  <td>i9</td>
-                  <td>SSD</td>
-                  <td>3TB</td>
-                  <td>Nvidia GTX 1060</td>
-                  <td>8GB</td>
-                  <td>32GB</td>
-                  <td>
-                    <button className="action-button edit">Edit</button>
-                    <button className="action-button delete">Delete</button>
-                  </td>
-                </tr>
+                {(laptopList || []).map((laptop) => {
+                  return (
+                    <tr key={laptop.id}>
+                      <td>{laptop.name}</td>
+                      <td>{laptop.processor}</td>
+                      <td>{laptop.memoryType}</td>
+                      <td>{laptop.memorySize}</td>
+                      <td>{laptop.gpu}</td>
+                      <td>{laptop.vram}</td>
+                      <td>{laptop.ram}</td>
+                      <td>
+                        <button className="action-button edit">Edit</button>
+                        <button className="action-button delete">Delete</button>
+                      </td>
+                    </tr>
+                  );
+                })}
               </tbody>
             </table>
           ) : (
-            <Form changeToggle={changeToggle} />
+            <Form changeToggle={changeToggle} onSubmit={addNewForm} />
           )}
         </div>
       </main>
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,30 +1,44 @@
+import { useState } from "react";
+
 // eslint-disable-next-line react/prop-types
-export function Form({ changeToggle, handleSubmit }) {
+export function Form({ changeToggle, onSubmit }) {
+  const [newForm, setNewForm] = useState({});
+
+  function handleInput(e) {
+    setNewForm({ ...newForm, [e.target.id]: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    onSubmit(newForm);
+
+    changeToggle();
+  }
+
   return (
     <>
       <section>
         <h1>Form</h1>
       </section>
       <div>
-        <form onSubmit={() => handleSubmit()}>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" />
+          <input type="text" id="name" onChange={handleInput} />
           <label htmlFor="processor">Processor</label>
-          <input type="text" id="processor" />
+          <input type="text" id="processor" onChange={handleInput} />
           <label htmlFor="memoryType">Memory type</label>
-          <input type="text" id="memoryType" />
+          <input type="text" id="memoryType" onChange={handleInput} />
           <label htmlFor="memorySize">Memory size</label>
-          <input type="text" id="memorySize" />
+          <input type="text" id="memorySize" onChange={handleInput} />
           <label htmlFor="gpu">GPU</label>
-          <input type="text" id="gpu" />
+          <input type="text" id="gpu" onChange={handleInput} />
           <label htmlFor="vram">VRAM</label>
-          <input type="text" id="vram" />
+          <input type="text" id="vram" onChange={handleInput} />
           <label htmlFor="ram">RAM</label>
-          <input type="text" id="ram" />
+          <input type="text" id="ram" onChange={handleInput} />
 
-          <button className="action-button edit" onClick={() => changeToggle()}>
-            Submit
-          </button>
+          <button className="action-button edit">Submit</button>
         </form>
       </div>
     </>
